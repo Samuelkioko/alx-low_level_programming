1aed46790ac72113f6162a1c4347dc2ff749c578
Samuelkioko <sammuelkioko99@gmail.com>
2022-03-10 23:14:21 +0300

    Number 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+
 /**
  * main - Entry point
+ *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	char buf[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n";
+	write(1,buf,strlen(buf));
 	return (1);
 }
