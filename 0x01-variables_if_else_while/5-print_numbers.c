5086b0dffb3a209ba83bd4fdc36ecdd68f3d171f
Samuelkioko <sammuelkioko99@gmail.com>
2022-03-11 13:24:43 +0300

    Number 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * a program that prints all single digit numbers of base 10 starting from 0, followed by a new line
+ * main - Entry point
+ *
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	return 0;
+}
+
