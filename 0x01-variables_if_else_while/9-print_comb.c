11b959c0832535bfb8ea36be53e7a38d9ab14ea6
Samuelkioko <sammuelkioko99@gmail.com>
2022-03-11 16:42:30 +0300

    Number 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+/*
+ * File: 9-print_comb.c
+ * Auth: Samuel Kioko
+ */
+#include <stdio.h>
+/**
+ * main - Start here
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}	
+	return (0);
+}
