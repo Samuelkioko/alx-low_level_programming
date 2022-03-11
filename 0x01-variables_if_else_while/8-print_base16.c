3fa878a27dcc36b21644ec35769f587766dcc598
Samuelkioko <sammuelkioko99@gmail.com>
2022-03-11 16:24:19 +0300

    Number 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+/*
+ * File: 8-print_base16.c
+ * Auth: Samuel Kioko
+ */
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16 in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char letter;
+	
+	for (n = 0; n <= 10; n++)
+		putchar((n % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+	return (0);
+}
+
