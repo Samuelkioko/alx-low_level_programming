2e9fd152edfb4c382302630a6ec79d21966a3e84
Samuelkioko <sammuelkioko99@gmail.com>
2022-03-11 14:25:19 +0300

    Number 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,18 +3,17 @@
 /**
  * main - Entry point
  *
- * return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int x= 'A';
-		do
-	{
+	int x = 'A';
+		do{
 		int low_x = tolower(x);
 		putchar(low_x);
 		x++;
 	}
-		while(x <= 'Z');
-	return 0;
+		while (x <= 'Z');
+	return (0);
 
 }
