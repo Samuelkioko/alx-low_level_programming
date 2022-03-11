3aa2f17b98e418fca5916e38cc12b2a1812d6e02
Samuelkioko <sammuelkioko99@gmail.com>
2022-03-11 17:39:13 +0300

    Last push

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+/*
+ * File: 102-print_comb5.c
+ * Auth: Samuel Kioko
+ */
+#include <stdio.h>
+/**
+ * main - Starts here
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 98; a++)
+	{
+		for (b = a + 1; b <= 99; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+
+			if (a == 98 && b == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
