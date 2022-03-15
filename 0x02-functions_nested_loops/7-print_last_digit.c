dc54ebd3be3fc22fdcfd6ec85638cfdae841d79e
Samuelkioko <sammuelkioko99@gmail.com>
2022-03-15 15:38:40 +0300

    Number 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,7 +14,7 @@ int print_last_digit(int n)
 
 	last_d = n % 10;
 
-	if (last_d < 0 ) /*convert to positive*/
+	if (last_d < 0 )/*convert to positive*/
 		last_d *= -1;
 	_putchar (last_d + '0');
 
