8f26ea944e254c7928f642de6c8ac4c654bde971
Samuelkioko <sammuelkioko99@gmail.com>
2022-03-15 21:55:53 +0300

    Number 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+/*
+ * File: 101-natural.c
+ * Auth: Samuel Kioko
+ */
+#include <stdio.h>
+/**
+ * main - list all the natural numbers below 10 that are multiples of 3 or 5
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int a, sum = 0;
+
+	for (a = 0; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5 == 0)
+				sum += a;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
