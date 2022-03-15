7f056cd71df0957ed0296dcdc7dfb30aa7202173
Samuelkioko <sammuelkioko99@gmail.com>
2022-03-15 14:35:32 +0300

    Number 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+/*
+ * File: 2-print_alphabet_x10.c
+ * Auth: Samuel Kioko
+ *
+ */
+#include "main.h"
+/**
+ * print_alphabet_x10 -  prints 10 times the alphabet, in lowercase, followed by a new line.
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int counter;
+	char x;
+
+	for (counter = 0; counter <= 9; counter++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+
+}
