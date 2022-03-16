e13654d2f3b1efaead5d327af6c00f7e3b39ee95
Samuelkioko <sammuelkioko99@gmail.com>
2022-03-17 01:50:12 +0300

    Second commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+/*
+ * File: main.c
+ * Auth: Samuel Kioko
+ */
+#include "main.h"
+/**
+ * main - Tests function
+ * Return: 0
+ */
+int main(void)
+{
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
