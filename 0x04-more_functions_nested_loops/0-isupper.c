97d1aba10b883e0046a4bdab5c87140cf5249486
Samuelkioko <sammuelkioko99@gmail.com>
2022-03-17 11:34:14 +0300

    Number 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+/*
+ * File: 0-isupper.c
+ * Auth: Samuel Kioko
+ */
+#include "main.h"
+/**
+ * main - check for upper case letter
+ * @c : character to check the case 
+ * 
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
