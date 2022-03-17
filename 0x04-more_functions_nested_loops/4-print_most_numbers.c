551b9f7d579e4eccd88916608ff76f7051472769
Samuelkioko <sammuelkioko99@gmail.com>
2022-03-17 12:24:08 +0300

    Number 5

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+/*
+ * File: 4-print_most_numbers.c
+ * Auth: Samuel Kioko
+ */
+#include "main.h"
+/**
+ * print_most_numbers - prints the numbers from 0 to 9,
+ * followed by a new line
+ *
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int x = 0;
+
+	do {
+		if (x != 2 && x != 4)
+		
+			_putchar((x % 10) + '0');
+		x++;
+	} while  (x <= 9);
+	_putchar('\n');
+}
