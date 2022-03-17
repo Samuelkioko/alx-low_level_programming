0a175ab80579ad124a38b1a91002dabb564a1128
Samuelkioko <sammuelkioko99@gmail.com>
2022-03-17 12:02:35 +0300

    Number 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+/*
+ * File: 2-mul.c
+ * Auth: Samuel Kioko
+ */
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: a*b
+ */
+int mul(int a, int b)
+{
+	return(a * b);
+}
