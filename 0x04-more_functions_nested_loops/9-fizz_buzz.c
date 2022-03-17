e5f1bb59d4bfbf9be4f3d8625ffa796bc107ed80
Samuelkioko <sammuelkioko99@gmail.com>
2022-03-17 13:22:01 +0300

    Number 10

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+/*
+ * File: 9-fizz_buzz.c
+ * Auth: Samuel Kioko
+ */
+#include <stdio.h>
+/**
+ * main - fizz-buzz test
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3) == 0 && (n % 5) == 0)
+			printf("FizzBuzz");
+		else if ((n % 3) == 0)
+			printf("Fizz");
+		else if ((n % 5) == 0)
+			printf("Buzz");
+		else
+			printf("%d", n);
+
+		if (n == 100)
+			continue;
+		printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
