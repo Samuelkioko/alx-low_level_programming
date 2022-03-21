6725a0efc75fda7af0ccf855a8fdd71944c810a5
Samuelkioko <sammuelkioko99@gmail.com>
2022-03-21 21:59:24 +0300

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+/*
+ * File: 7-puts_half.c
+ * Auth: Samuel Kioko
+ */
+#include "main.h"
+/**
+ * puts_half - prints half of string,
+ * followed by a new line
+ * @str: pointer
+ * Return: Void
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+	for (int i = (len + 1) / 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
