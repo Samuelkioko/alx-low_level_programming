5d8f9c1d02b31c72375b74e3233be12b593a0172
Samuelkioko <sammuelkioko99@gmail.com>
2022-03-21 22:48:36 +0300

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,16 +11,18 @@
  */
 void puts_half(char *str)
 {
-	int len = 0;
-	int i = (len + 1) / 2;
-
+	int len = 0, i, n;
+	
 	while (str[len] != '\0')
 		len++;
 
-	while (str[i] != '\0')
-	{
+	if (len % 2 == 0)
+		n = len / 2;
+	else
+		n = (len + 1) / 2;
+
+	for (i = n; i < len; i++)
 		_putchar(str[i]);
-		i++;
-	}
+
 	_putchar('\n');
 }
