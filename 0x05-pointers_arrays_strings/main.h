9bce5c1930a6b6ff5ff1e086d356b6f99b81dce0
Samuelkioko <sammuelkioko99@gmail.com>
2022-03-21 20:53:27 +0300

    Task 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,8 +5,19 @@
  * File: main.h
  * Auth: Samuel Kioko
  * Desc: Header file containing declarations for all functions
- *       used in the 0x02-functions_nested_loops directory.
+ *       used in the 0x05-pointer_arrays_strings directory.
  */
 int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif
